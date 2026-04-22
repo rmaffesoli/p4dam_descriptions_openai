fde334af85e970e77f8abf8649fbc967f3c847b8
Ryan Maffesoli <pairorat@gmail.com>
2026-04-22 14:39:20 -0400

    more simplification

--- start_container.sh
+++ start_container.sh
@@ -14,8 +14,6 @@ docker run -d \
   -e DAM_SERVER_ADDRESS="http://10.0.0.1" \ # Change this to the private IP address of DAM
   -e DAM_ACCOUNT_KEY="your_account_key_here" \
   -e OPENAI_API_KEY="your_api_key_here" \
-  -e P4PORT="your_p4_port_here" \
-  -e P4USER="your_p4_user_here"
   openai-descriptions-webhook
 
 echo "Container started"
